84ef1574eae00bfb486c1f18e30b47158a53e726
adminoryuan <1317332365@qq.com>
2022-05-05 09:48:21 +0800

    jwtuntity

---
+++ src/main/java/com/HyChat/server/untity/JwtUntity.java
@@ -0,0 +1,13 @@
+package com.HyChat.server.untity;
+
+import com.HyChat.server.Entity.User;
+
+/**
+ * 增加jwt 校验
+ */
+public class JwtUntity {
+
+    public static String EncoderJwt(User user){
+        return "";
+    }
+}
